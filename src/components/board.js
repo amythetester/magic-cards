f040e545e3d4bbe482a63a4e69152bf56dbea66b
Amy <36608155+amythetester@users.noreply.github.com>
2019-12-05 17:32:53 -0800

    Basic Homepage (#1)
    * added homepage
    
    * added nav
    
    * add card board
    
    * add banner
    
    * update nav using bootstrap

---
+++ src/components/board.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Board() {
+  return (
+    <div className="Board">
+      Magical Beasts and Where to Find Them
+    </div>
+  );
+}
+
+export default Board;
---
+++ src/homepage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Board from './components/board.js';
+import Nav from './components/nav.js';
+
+import './homepage.css'
+
+function Homepage() {
+  return (
+    <div className="Homepage">
+      <Nav />
+      <section className="hero">
+        <div className="transparent">
+          <h1>Magical Beasts and Where to Find Them</h1>
+          <h3>Scroll to see what's out there.</h3>
+        </div>
+      </section>
+      <Board />
+    </div>
+  );
+}
+
+export default Homepage;
