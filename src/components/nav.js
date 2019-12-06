a7e7455d5f9aafafbfb1ba58d937264bf718d16f
Amy Wyatt <amy.wyatt12@outlook.com>
2019-12-05 23:01:07 -0800

    back to top

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,10 +3,16 @@ import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-dark bg-dark">
+    <nav className="navbar sticky-top navbar-light bg-light">
       <Link to="/" className="navbar-brand">Magical Creatures</Link>
+      <Link to={goToTop}>Back to Top</Link>
     </nav>
   );
+}
+
+function goToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 export default Nav;
