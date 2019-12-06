1166a78cbebdb8bc841fcabe1ab419e830eeb76c
Amy Wyatt <amy.wyatt12@outlook.com>
2019-12-05 21:52:16 -0800

    rendering images on cards

--- src/components/card.js
+++ src/components/card.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 function Card(props) {
   return (
-    <div className="Card">
-      {props.image}
-      {props.name}
-      {props.artist}
-      {props.set}
-      {props.type}
-
+    <div className="card">
+      <section className="card__content">
+        {props.image && <img src={props.image} alt="creature card" className="card__content-image"/>}
+        {props.name && <div className="card__content-name">Name: {props.name}</div>}
+        {props.artist && <div className="card__content-artist">Artist: {props.artist}</div>}
+        {props.set && <div className="card__content-set">Set: {props.set}</div>}
+        {props.type && <div className="card__content-type">Original Type: {props.type}</div>}
+      </section>
     </div>
   );
 }
