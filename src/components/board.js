0f1c940352a2adb91e67fc6501f04589ac1aca38
Amy Wyatt <amy.wyatt12@outlook.com>
2019-12-05 18:53:08 -0800

    add sdk and make initial API call

--- src/components/board.js
+++ src/components/board.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Board() {
-  return (
-    <div className="Board">
-      Magical Beasts and Where to Find Them
-    </div>
-  );
+class Board extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cards: []
+    };
+  }
+
+  componentDidMount() {
+    this.getInitialCards();
+  }
+
+  getInitialCards = () => {
+    const mtg = require('mtgsdk')
+
+    mtg.card.where({ types: 'creature'})
+    .then(cards => {
+      this.setState = ({cards: cards})
+      console.log(cards) 
+    })
+  }
+
+  render() {
+    return (
+      <div className="Board">
+        Magical Beasts and Where to Find Them
+      </div>
+    );
+  }
 }
 
 export default Board;
