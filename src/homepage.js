72687832010abd3835797576fb91d5e07c372c4b
Amy <36608155+amythetester@users.noreply.github.com>
2019-12-07 10:35:49 -0800

    Refactor Banner (#5)

--- src/homepage.js
+++ src/homepage.js
@@ -1,20 +1,14 @@
 import React from 'react';
 
+import Banner from './components/banner.js'
 import Board from './components/board.js';
 import Nav from './components/nav.js';
-
-import './homepage.css'
 
 function Homepage() {
   return (
     <div className="Homepage">
       <Nav />
-      <section className="hero">
-        <div className="transparent">
-          <h1>Magical Beasts and Where to Find Them</h1>
-          <h3>Scroll to see what's out there.</h3>
-        </div>
-      </section>
+      <Banner />
       <Board />
     </div>
   );
