d057f5037458fdfa55bd0120a4f0a349207c12f6
Amy Wyatt <amy.wyatt12@outlook.com>
2019-12-07 10:33:20 -0800

    make banner component

---
+++ src/components/banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './banner.css'
+
+function Banner() {
+  return (
+    <div className="hero">
+      <div className="transparent">
+        <h1>Magical Beasts and Where to Find Them</h1>
+        <h3>Scroll to see what's out there.</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
