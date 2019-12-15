7d67aad93216d4f42233a6a97f2367be97691e66
Amy Wyatt <amy.wyatt12@outlook.com>
2019-12-14 19:18:00 -0800

    realigned search items

--- src/components/search.js
+++ src/components/search.js
@@ -39,7 +39,7 @@ class Search extends Component {
             <input id="search-input" type="text" placeholder="Card name..." value={this.state.value} onChange={this.handleChange} />
             <button id="search-button" className="btn btn-light">Search</button>
         </form>
-        <h5 className="label">Current Search: {this.state.mostRecent}</h5>
+        <h5 className="last-search">Current Search: {this.state.mostRecent}</h5>
       </div>
     );
   }
